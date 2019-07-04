9bb6d9fc19c0d67380029d45e51ccbda1914c03e
xiexushuang <xushuang.xie@zuolin.com>
2019-07-04 18:33:13 +0800

    新增测试类

---
+++ src/test/java/com/xxs/TestDemo.java
@@ -0,0 +1,11 @@
+package com.xxs;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+
+public class TestDemo{
+	@Test
+	public void test0(){
+		System.out.println(System.getProperty("user.dir"));
+	}
+}
